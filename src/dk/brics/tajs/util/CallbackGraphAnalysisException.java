5a51133bcf7f2f92be7b827d2fd822a8afd9ca59
BernardoFuret <bernardo.furet@tecnico.ulisboa.pt>
2020-05-05 23:46:34 +0100

    Exception for the Callback graph analysis.

---
+++ src/dk/brics/tajs/util/CallbackGraphAnalysisException.java
@@ -0,0 +1,17 @@
+package dk.brics.tajs.util;
+
+/**
+ * Exception for CallbackGraphAnalysis.
+ */
+public class CallbackGraphAnalysisException extends AnalysisException {
+
+	private static final long serialVersionUID = 2020L;
+
+	/**
+	 * Constructs a new exception.
+	 */
+	public CallbackGraphAnalysisException( String msg ) {
+		super( msg );
+	}
+
+}
